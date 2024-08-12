63b0f5beb3ba83a3fd843cffc82546764d28a5d6
AxelSalim <adjakidjememiaghe@gmail.com>
2024-08-12 18:20:10 +0100

    modification du header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
 
   return (
       <section className=" h-[95%]">
-          <Image src="/1x/''Fichier 6.png" width={1000} height={1000} alt="Banner Image" className="absolute w-full object-cover inset-0 z-[-1]"/>
+          <Image src="/1x/''Fichier 2.png" width={1000} height={1000} alt="Banner Image" className="absolute w-full object-cover inset-0 z-[-1]"/>
       </section>
   );
 }
