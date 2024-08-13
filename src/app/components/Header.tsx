8bfcbccaae523949b568fb57b689b0ea9e19970c
AxelSalim <adjakidjememiaghe@gmail.com>
2024-08-13 16:22:38 +0100

    suppression de la hauteur arbitraire

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
-import { useEffect, useState } from 'react';
-import { Navbar } from "./Navbar";
 
 
 export default function Header() {
 
   return (
-      <section className=" h-[95%]">
-          <Image src="/1x/''Fichier 2.png" width={1000} height={1000} alt="Banner Image" className="absolute w-full object-cover inset-0 z-[-1]"/>
+      <section className="">
+          <Image src="/public/1x/''Fichier 2.png" width={1000} height={1000} alt="Banner Image" className=""/>
       </section>
   );
 }
