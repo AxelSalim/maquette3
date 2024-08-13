77fdbb3286d08967f325fffd7a01202357a0d78a
AxelSalim <adjakidjememiaghe@gmail.com>
2024-08-13 16:21:42 +0100

    implémentation de la fonction pour le scroll de la page

--- src/app/accueil.tsx
+++ src/app/accueil.tsx
@@ -8,30 +8,59 @@ import { Navbar } from "./components/Navbar";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import ScrollToTopButton from "./components/ScrollToTopButton";
 
 
 const Accueil: React.FC<{ childrens: React.ReactNode }> = ({ childrens }) => {
   const [loading, setLoading] = useState(true);
   const [fadeOutLoading, setFadeOutLoading] = useState(false);
+  const [isNavSticky, setIsNavSticky] = useState(false);
 
 
-//   if (loading) {
-//     return (
-//       <html>
-//         <head>
-//           <title>BrainWave</title>
-//           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5"/>
-//           <ThemeModeScript />
-//         </head>
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      setIsNavSticky(scrollTop > 0); // Met à jour l'état en fonction du défilement
+    };
 
-//         <body className={`${poppins.className}`}>
-//           <div className={`loading-screen ${fadeOutLoading ? 'fade-out' : ''}`}>
-//             <span className="loader"></span>
-//           </div>
-//         </body>
-//       </html>
-//     );
-//   }
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setFadeOutLoading(true);
+      setTimeout(() => setLoading(false), 500);
+    };
+
+    fetchData();
+  }, []);
+
+
+  if(loading) {
+    return (
+      <html>
+        <head>
+          <title>BrainWave</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5"/>
+          <ThemeModeScript />
+        </head>
+
+        <body className={`${poppins.className}`}>
+          <div className={`loading-screen ${fadeOutLoading ? 'fade-out' : ''}`}>
+          <div className="cube-loader absolute bottom-0 left-0">
+            <div className="cube cube1"></div>
+            <div className="cube cube2"></div>
+            <div className="cube cube3"></div>
+            <div className="cube cube4"></div>
+          </div>
+
+          </div>
+        </body>
+      </html>
+    );
+  }
 
   return (
     <html>
@@ -42,8 +71,7 @@ const Accueil: React.FC<{ childrens: React.ReactNode }> = ({ childrens }) => {
       </head>
 
       <body className={`content ${poppins.className}`}>
-        <header className="">
-            <Header></Header>
+        <header className="absolute z-50 w-full">
             <Navbar></Navbar>
         </header>
 
@@ -54,7 +82,9 @@ const Accueil: React.FC<{ childrens: React.ReactNode }> = ({ childrens }) => {
         <footer className="">
             <Footer />
         </footer>
+        <ScrollToTopButton/>
       </body>
+
     </html>
   );
 };
